a259b409d71fc5265635c322474b711a486d7aa7
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-06-19 17:30:14 -0300

    refactor: params api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,21 +2,22 @@ import axios from 'axios';
 
 const apiKey = '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z';
 
+const params = { hl: 'pt-BR' };
+
 export const api = axios.create({
   baseURL: 'https://esports-api.lolesports.com/persisted/gw',
   headers: {
     'x-api-key': apiKey
   },
-  params: {
-    hl: 'pt-BR'
-  }
+  params
 });
 
 export const apiGame = axios.create({
   baseURL: 'https://feed.lolesports.com/livestats/v1',
   headers: {
     'x-api-key': apiKey
-  }
+  },
+  params
 });
 
 export default api;
