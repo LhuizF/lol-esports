a80e31662019828ec78169d5128957d8e6bf8577
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-06-20 22:55:24 -0300

    feat: add scoreboard content

--- src/components/Molecules/Scoreboard/index.tsx
+++ src/components/Molecules/Scoreboard/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useEffect, useState } from 'react';
+import DetailsIcon from '../../Atoms/DetailsIcon';
+import { Container, Content, Text } from './styles';
 
 interface Props {
   redTeam: TeamFrame;
@@ -7,7 +8,53 @@ interface Props {
 }
 
 const Scoreboard: React.FC<Props> = ({ blueTeam, redTeam }) => {
-  return <Container></Container>;
+  const [redGold, setRedGold] = useState<number>(0);
+  const [blueGold, setBlueGold] = useState<number>(0);
+
+  useEffect(() => {
+    SetGold(blueGold, blueTeam.totalGold, setBlueGold);
+  }, [blueTeam, redTeam]);
+
+  const SetGold = (gold: number, newGold: number, setNewGold: any): void => {
+    while (gold < newGold) {
+      gold += 1;
+      setNewGold(gold);
+    }
+  };
+
+  return (
+    <Container>
+      <Content>
+        <DetailsIcon name="inhibitor" color="blue" value={redTeam.inhibitors} />
+        <DetailsIcon name="baron" color="blue" value={blueTeam.barons} />
+        <DetailsIcon name="towers" color="blue" value={blueTeam.towers} />
+        <DetailsIcon
+          name="coins"
+          color="blue"
+          value={+blueTeam.totalGold.toLocaleString('pt-BR')}
+        />
+      </Content>
+
+      <Text>
+        <div>
+          <p>{blueTeam.totalKills}</p>
+          <DetailsIcon name="kills" size={50} />
+          <p>{redTeam.totalKills}</p>
+        </div>
+      </Text>
+
+      <Content>
+        <DetailsIcon
+          name="coins"
+          color="red"
+          value={+redTeam.totalGold.toLocaleString('pt-BR')}
+        />
+        <DetailsIcon name="towers" color="red" value={redTeam.towers} />
+        <DetailsIcon name="baron" color="red" value={redTeam.barons} />
+        <DetailsIcon name="inhibitor" color="red" value={redTeam.inhibitors} />
+      </Content>
+    </Container>
+  );
 };
 
 export default Scoreboard;
--- src/components/Molecules/Scoreboard/styles.ts
+++ src/components/Molecules/Scoreboard/styles.ts
@@ -1,7 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-top: 2rem;
+  margin-top: 1.5rem;
   background-color: ${({ theme }) => theme.colors.greySecondary};
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+  align-items: flex-end;
+`;
+
+export const Text = styled.div`
+  font-size: 1.8rem;
+  display: flex;
+  align-items: flex-start;
+  color: ${({ theme }) => theme.colors.white};
+
+  div {
+    display: flex;
+    align-items: center;
+
+    p {
+      margin: 0 2rem;
+    }
+  }
 `;
