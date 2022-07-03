45809c407c730e431d0cfdfb59cf0824c0da57e9
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-07-03 14:31:36 -0300

    feat: add styles file

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ItemContainer = styled.div`
+  margin: 0 2px;
+  position: relative;
+  height: 40px;
+
+  p {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    line-height: 16px;
+  }
+`;
