60b7079ece37a808f940766b80ab1f3340fba5bf
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-07-23 19:51:42 -0300

    feat: use WindowSize in Icon

--- src/components/Atoms/DetailsIcon/index.tsx
+++ src/components/Atoms/DetailsIcon/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from './styles';
 import { detailsIcons, colors } from '../../../utils/svg';
+import useWindowSize from '../../../hooks/useWindowSize';
 
 export type ColorsType = keyof typeof colors;
 export type IconsType = keyof typeof detailsIcons;
@@ -13,12 +14,24 @@ interface Props {
 }
 
 const DetailsIcon: React.FC<Props> = ({ name, color, size, value }) => {
+  const [currentSize, setCurrentSize] = useState(0);
+  const { width } = useWindowSize();
+
+  useEffect(() => {
+    const newSize = size ? size : 30;
+    if (width < 680) {
+      setCurrentSize(newSize / 1.8);
+      return;
+    }
+    setCurrentSize(newSize);
+  }, [width, size]);
+
   return (
     <Container>
       <svg
         className="icon"
-        width={size ? size : 30}
-        height={size ? size : 30}
+        width={currentSize}
+        height={currentSize}
         viewBox="0 0 24 24"
         xmlns="https://www.w3.org/2000/svg"
       >
--- src/components/Atoms/DragonIcon/index.tsx
+++ src/components/Atoms/DragonIcon/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from './styles';
 import { dragonsIcons as icons } from '../../../utils/svg';
+import useWindowSize from '../../../hooks/useWindowSize';
 
 export type Type = keyof typeof icons;
 
@@ -9,12 +10,23 @@ interface Props {
 }
 
 const DragonIcon: React.FC<Props> = ({ dragon }) => {
+  const [mobile, setMobile] = useState(false);
+  const { width } = useWindowSize();
+
+  useEffect(() => {
+    if (width < 690) {
+      setMobile(true);
+      return;
+    }
+    setMobile(false);
+  }, [width]);
+
   return (
     <Container title={icons[dragon].name}>
       <svg
         className="icon"
-        width={30}
-        height={30}
+        width={mobile ? 15 : 30}
+        height={mobile ? 15 : 30}
         viewBox="0 0 24 24"
         xmlns="https://www.w3.org/2000/svg"
       >
